4fffcbe64b6e649bc3d5a94c9fd73f780bdfd340
Pierre Tissot <pierregabriel@me.com>
2019-01-31 11:46:44 +0100

    add clever_cloud template

---
+++ clever_cloud/devise.rb
@@ -0,0 +1,269 @@
+run 'pgrep spring | xargs kill -9'
+
+# GEMFILE
+########################################
+run 'rm Gemfile'
+file 'Gemfile', <<-RUBY
+source 'https://rubygems.org'
+ruby '#{RUBY_VERSION}'
+
+#{"gem 'bootsnap', require: false" if Rails.version >= "5.2"}
+gem 'devise'
+gem 'jbuilder', '~> 2.0'
+gem 'pg', '~> 0.21'
+gem 'puma'
+gem 'rails', '#{Rails.version}'
+gem 'redis'
+
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'sassc-rails'
+gem 'simple_form'
+gem 'uglifier'
+gem 'webpacker'
+gem 'slim-rails'
+gem 'devise-i18n'
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+end
+
+group :development, :test do
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'dotenv-rails'
+end
+RUBY
+
+# Ruby version
+########################################
+file '.ruby-version', RUBY_VERSION
+
+# Procfile
+########################################
+file 'Procfile', <<-YAML
+web: bundle exec puma -C config/puma.rb
+YAML
+
+# Clevercloud conf file
+########################################
+file 'clevercloud/ruby.json', <<-EOF
+{
+  "deploy": {
+    "rakegoals": ["assets:precompile", "db:migrate"]
+  }
+}
+EOF
+
+# Database conf file
+########################################
+inside 'config' do
+  database_conf = <<-EOF
+default: &default
+  adapter: postgresql
+  encoding: unicode
+  pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
+
+development:
+  <<: *default
+  database: #{app_name}_development
+
+test:
+  <<: *default
+  database: #{app_name}_test
+
+production:
+  <<: *default
+  url: <%= ENV['POSTGRESQL_ADDON_URI'] %>
+EOF
+  file 'database.yml', database_conf, force: true
+end
+
+# Assets
+########################################
+run 'rm -rf app/assets/stylesheets'
+run 'rm -rf vendor'
+run 'curl -L https://github.com/Pagehey/rails-stylesheets/archive/master.zip > stylesheets.zip'
+run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets'
+
+run 'rm app/assets/javascripts/application.js'
+file 'app/assets/javascripts/application.js', <<-JS
+//= require rails-ujs
+//= require_tree .
+JS
+
+# Dev environment
+########################################
+gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
+
+# Layout
+########################################
+run 'rm app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.slim', <<-SLIM
+doctype html
+html
+  head
+    meta charset="UTF-8"
+    meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    title TODO
+    = csrf_meta_tags
+    = action_cable_meta_tag
+    = stylesheet_link_tag 'application', media: 'all'
+    /= stylesheet_pack_tag 'application', media: 'all'
+  body
+    = render 'shared/flashes'
+    = yield
+    = javascript_include_tag 'application'
+    = javascript_pack_tag 'application'
+SLIM
+
+file 'app/views/shared/_flashes.html.slim', <<-SLIM
+- if notice
+  .alert.alert-info.alert-dismissible role="alert"
+    button.close type="button" data-dismiss="alert" aria-label="Close"
+      span aria-hidden="true" &times;
+    = notice
+- if alert
+  .alert.alert-warning.alert-dismissible role="alert"
+    button.close type="button" data-dismiss="alert" aria-label="Close"
+      span aria-hidden="true" &times;
+    = alert
+SLIM
+
+# README
+########################################
+markdown_file_content = <<-MARKDOWN
+Rails app inspired by [lewagon/rails-templates](https://github.com/lewagon/rails-templates).
+MARKDOWN
+file 'README.md', markdown_file_content, force: true
+
+# Generators
+########################################
+generators = <<-RUBY
+config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework  :test_unit, fixture: false
+    end
+RUBY
+
+# DEFAULT_LOCALE
+########################################
+  application do <<-EOF
+    config.i18n.default_locale = :fr
+EOF
+  end
+
+environment generators
+
+########################################
+# AFTER BUNDLE
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command 'db:drop db:create db:migrate'
+  generate('simple_form:install', '--bootstrap')
+  generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
+
+  # Routes
+  ########################################
+  route "root to: 'pages#home'"
+
+  # Git ignore
+  ########################################
+  run 'rm .gitignore'
+  file '.gitignore', <<-TXT
+.bundle
+.clever.json
+log/*.log
+tmp/**/*
+tmp/*
+!log/.keep
+!tmp/.keep
+*.swp
+.DS_Store
+public/assets
+public/packs
+public/packs-test
+node_modules
+yarn-error.log
+.byebug_history
+.env*
+TXT
+
+  # Devise install + user
+  ########################################
+  generate('devise:install')
+  generate('devise', 'User')
+
+  # App controller
+  ########################################
+  run 'rm app/controllers/application_controller.rb'
+  file 'app/controllers/application_controller.rb', <<-RUBY
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+end
+RUBY
+
+  # migrate + devise views
+  ########################################
+  rails_command 'db:migrate'
+  generate('devise:i18n:views')
+
+  # Pages Controller
+  ########################################
+  run 'rm app/controllers/pages_controller.rb'
+  file 'app/controllers/pages_controller.rb', <<-RUBY
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
+
+  def home
+  end
+end
+RUBY
+
+  # Environments
+  ########################################
+  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
+  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
+
+  # Webpacker / Yarn
+  ########################################
+  run 'rm app/javascript/packs/application.js'
+  run 'yarn add jquery bootstrap@4.2.1 popper.js'
+  file 'app/javascript/packs/application.js', <<-JS
+import "bootstrap";
+JS
+
+  inject_into_file 'config/webpack/environment.js', before: 'module.exports' do
+<<-JS
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+  })
+)
+
+JS
+  end
+
+  # Dotenv
+  ########################################
+  run 'touch .env'
+
+  # Rubocop
+  ########################################
+  run 'curl -L https://raw.githubusercontent.com/Pagehey/rails-templates/master/.rubocop.yml > .rubocop.yml'
+
+  # Git
+  ########################################
+  git :init
+  git add: '.'
+  git commit: "-m 'Initial commit'"
+end
